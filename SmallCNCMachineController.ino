aff58d129f4ffde55acf53e098e4a33c869fbe3a
detlefgrohs <detlef.grohs@gmail.com>
2016-07-01 04:17:25 -0400

    Adding movement logic

--- SmallCNCMachineController.ino
+++ SmallCNCMachineController.ino
@@ -1,3 +1,4 @@
+#include <LinkedList.h>
 #include <TaskScheduler.h>
 
 /*
@@ -8,7 +9,7 @@
 
 const int DelayMSecs = 1;
 
-const int LedPin = 13;
+const int HeartbeatLedPin = 13;
 
 const int MotorEnablePin = 12;
 
@@ -23,6 +24,11 @@ const int Backward = 1;
 const int XAxis = 0;
 const int YAxis = 1;
 
+// ======================================================================
+
+// TaskScheduler 
+Scheduler TaskRunner;
+
 // Callback Prototypes
 void SerialController();
 void Controller();
@@ -35,11 +41,26 @@ Task ControllerTask(250, TASK_FOREVER, &Controller);
 Task XAxisControllerTask(1, TASK_FOREVER, &XAxisController);
 Task YAxisControllerTask(1, TASK_FOREVER, &YAxisController);
 
-Scheduler TaskRunner;
+// ======================================================================
+
+// Message Handling Functioning
+String CurrentMessage = "";
+LinkedList<String> *MessageQueue = new LinkedList<String>();
+
+// ======================================================================
+
+// Movement stuff...
+int XAxisStepCurrentPosition = 0;
+int XAxisStepDesiredPosition = 0;
+//int XAxisDir = Forward;
+int XAxisRate = 1;
+int XAxisCount = 0;
+
+// ======================================================================
 
 void setup() {
 
-  pinMode(LedPin, OUTPUT);
+  pinMode(HeartbeatLedPin, OUTPUT);
 
   pinMode(MotorEnablePin, OUTPUT);
 
@@ -58,7 +79,7 @@ void setup() {
   digitalWrite(YAxisStpPin, LOW);
 
   // Setup the Serial communications
-  Serial.begin(9600);   // What should this actually be?
+  Serial.begin(115200);   // What should this actually be?
   while (!Serial)
     ; // Wait for the serial port to connect
 
@@ -80,37 +101,26 @@ void loop() {
 }
 
 void SerialController() {
-    while (Serial.available()) {  
-      int inChar = Serial.read();
+    while (Serial.available()) {
+      char inChar = Serial.read();
 
-      switch(inChar) {
-        case char('O'):
-          Serial.println("Enable Motors");
-          digitalWrite(MotorEnablePin, LOW);
-          break;
-        case char('o'):
-          Serial.println("Disable Motors");
-          digitalWrite(MotorEnablePin, HIGH);
-          break;
-        case char('X'):
-          Serial.println("Jog X Forward");
-          StepXAxis(Forward);
-          break;
-        case char('x'):
-          Serial.println("Jog X Backward");
-          StepXAxis(Backward);
-          break;
-        case char('L'):
-          Circuit();
-          break;
-        case char('V'):
-          Serial.println("Version Beta Mark 1");
-          break;
-        default:
-          Serial.print("Unprocessed: ");
-          Serial.println(inChar);
-          break;
+      if ((inChar == 10) || (inChar == 13))
+      {
+        if (CurrentMessage.length() > 0) {
+          // Push the message to the end of the queue
+          if (CurrentMessage.startsWith("M126")) {
+            // Emergency Stop
+            MessageQueue->clear();
+            Serial.println("Emergency Stop.");
+          } else {
+            MessageQueue->add(CurrentMessage);
+          }
+          CurrentMessage = "";
+        }
       }
+      else
+        // Append the current character to the current message
+        CurrentMessage += String(inChar);
   }
 }
 
@@ -125,20 +135,97 @@ void Circuit() {
     StepXAxis(Backward);
   for(int count = steps; count > 0; count--)
     StepYAxis(Backward);
-    
-  
 }
 
 void Controller() {
-  if (digitalRead(LedPin))
-    digitalWrite(LedPin, LOW);
+  // Heartbeat
+  if (digitalRead(HeartbeatLedPin))
+    digitalWrite(HeartbeatLedPin, LOW);
   else
-    digitalWrite(LedPin, HIGH);
-    
+    digitalWrite(HeartbeatLedPin, HIGH);
+
+  if (MessageQueue->size() > 0 && !IsMachineMoving()) {
+    // Pop the first message in the message queue
+    String message = MessageQueue->remove(0);
+
+    if (message.startsWith("M0")) {
+      Serial.println("Motors off.");
+    } else if (message.startsWith("M114")){
+      Serial.print("X:");
+      Serial.print(XAxisStepCurrentPosition);
+      Serial.print(" Y:");
+      Serial.print(0);
+      Serial.println();
+    } else if (message.startsWith("M1")){
+      Serial.println("Motors on.");
+    } else if (message.startsWith("G0")){
+      int moveX = message.substring(message.indexOf("X") + 1).toInt();
+      int moveY = message.substring(message.indexOf("Y") + 1).toInt();
+      
+      Serial.print("Rapid Move X:");
+      Serial.print(moveX);
+      Serial.print(" Y:");
+      Serial.print(moveY);
+      Serial.println();
+
+      XAxisStepDesiredPosition = moveX;
+      XAxisCount = 0;
+      XAxisRate = 1;
+    } else if (message.startsWith("G1")){
+      int moveX = message.substring(message.indexOf("X") + 1).toInt();
+      int moveY = message.substring(message.indexOf("Y") + 1).toInt();
+      
+      Serial.print("Move X:");
+      Serial.print(moveX);
+      Serial.print(" Y:");
+      Serial.print(moveY);
+      Serial.println();
+
+      XAxisStepDesiredPosition = moveX;
+      XAxisCount = 0;
+      XAxisRate = 100;
+    } else {
+      Serial.print("Unprocessed Message: '");
+      Serial.print(message);
+      Serial.println("'");
+    }
+  }
+}
+
+boolean IsMachineMoving() {
+  if (XAxisStepDesiredPosition != XAxisStepCurrentPosition)
+    return true;
+
+  return false;
 }
 
 void XAxisController() {
+  if (XAxisStepDesiredPosition != XAxisStepCurrentPosition) {
+    XAxisCount--; // Decrement the count so we know the next pulse that we need...
+    if (XAxisCount <= 0) {
+      XAxisCount = XAxisRate; // Set the count to the rate...
 
+      // Set the direction
+      if ((XAxisStepDesiredPosition > XAxisStepCurrentPosition) && digitalRead(XAxisDirPin))
+        digitalWrite(XAxisDirPin, LOW);
+      if ((XAxisStepDesiredPosition < XAxisStepCurrentPosition) && !digitalRead(XAxisDirPin))
+        digitalWrite(XAxisDirPin, HIGH);
+
+      //
+      if (digitalRead(XAxisStpPin)) {
+        digitalWrite(XAxisStpPin, LOW);
+        Serial.println("XAxis Low");
+
+        if (XAxisStepDesiredPosition > XAxisStepCurrentPosition)
+          XAxisStepCurrentPosition ++;
+        else
+          XAxisStepCurrentPosition --;
+      } else {
+        digitalWrite(XAxisStpPin, HIGH);
+        Serial.println("XAxis High");
+      }
+    }
+  }
 }
 
 void YAxisController() {
